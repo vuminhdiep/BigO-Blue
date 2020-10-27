885275fbfb4b2db16cb0b7ac9b2fbf13f0eb99ca
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-10-27 16:43:09 +0700

    Update 27/10

--- lec1.py
+++ lec1.py
@@ -1,29 +1,32 @@
-#Fashion
-def check_jacket(v, n):
-    # 1-button jacket
-    if n == 1:
-        if v[0] == 1:
-            return True
-        else:
-            return False
-    count = 0
-    for i in range(n):
-        if v[i] == 0:
-            count += 1
+# Fashion
+#
+# def check_jacket(v, n):
+#     # 1-button jacket
+#     if n == 1:
+#         if v[0] == 1:
+#             return True
+#         else:
+#             return False
+#     count = 0
+#     for i in range(n):
+#         if v[i] == 0:
+#             count += 1
+#
+#     if count == 1:
+#         return True
+#     else:
+#         return False
+#
+#
+# n = int(input())
+# v = list(map(int, input().split()))
+# if check_jacket(v, n):
+#     print("YES")
+# else:
+#     print("NO")
 
-    if count == 1:
-        return True
-    else:
-        return False
 
-
-n = int(input())
-v = list(map(int, input().split()))
-if check_jacket(v, n):
-    print("YES")
-else:
-    print("NO")
-
+# Night at the Museum
 
 def main():
     n = input()
@@ -42,8 +45,29 @@ def main():
             curr = x
         else:
             break
-    print(str(res) + '\n')
+
+    print(res)
 
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
+
+# Bear and Game
+
+n = int(input())
+v = list(map(int, input().split()))
+
+start = 0
+watch = 0
+for i in v:
+    if i - start <= 15:
+        watch = i
+        start = i
+    else:
+        break
+
+print(min(90, watch + 15))
+
+# Vitaly and Strings
+str1 = input()
+str2 = input()
