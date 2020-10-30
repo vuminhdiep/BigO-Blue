dc3c124125fc4928cd0bd852eeed4692079737c0
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-10-30 14:15:10 +0700

    Update arrays.py

--- arrays.py
+++ arrays.py
@@ -1,10 +1,25 @@
-nA, nB = (map(int, input().split()))
-k, m = (map(int, input().split()))
+# nA, nB = (map(int, input().split()))
+# k, m = (map(int, input().split()))
+#
+# listA = list(map(int, input().split()))
+# listB = list(map(int, input().split()))
+#
+# if listA[k-1] < listB[nB-m]: #if first k numbers in A < last m number in B
+#     print("YES")
+# else:
+#     print("NO")
 
-listA = list(map(int, input().split()))
-listB = list(map(int, input().split()))
+n = int(input())
+m = []
+for i in range(n):
+    a, b = map(int, input().split())
+    m.append([a, b])
 
-if listA[k-1] < listB[nB-m]: #if first k numbers in A < last m number in B
-    print("YES")
+min_val = min([x[0] for x in m]) #Get the smallest value possible in the segments
+max_val = max([x[1] for x in m]) #Get the biggest value possible in the segments
+seg = [min_val, max_val] #The segment with smallest and biggest value will cover all segments in between
+if seg in m:
+    print(m.index(seg) + 1)
 else:
-    print("NO")
+    print(-1)
+
