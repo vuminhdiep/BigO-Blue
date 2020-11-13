adcfeaefd33e2ade09121bdda92a3c0638a5aa68
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-11-13 12:35:58 +0700

    Update lec4.py

--- lec4.py
+++ lec4.py
@@ -1,1 +1,22 @@
-#Transform the Expression
+# Transform the Expression
+t = int(input())
+expression = []
+
+for i in range(t):
+    expression.append(input().strip())
+print(expression)
+
+res = ''
+stack = []
+
+for express in expression:
+    for char in express:
+        if char.isalpha():
+            res += char
+        elif char == ')':
+            res += stack[-1]
+            stack.pop()
+        elif char != '(':
+            stack.append(char)
+
+print(res)
