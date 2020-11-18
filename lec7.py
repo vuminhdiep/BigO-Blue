04a381192e62317a73023d20681896c639cdfe3c
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-11-18 16:41:26 +0700

    Update lec7.py

--- lec7.py
+++ lec7.py
@@ -1,4 +1,4 @@
-#Monk and Multiplication
+# Monk and Multiplication
 import heapq
 
 N = int(input())
@@ -29,3 +29,29 @@ for i in arr:
     heapq.heappush(heap, second_largest)
 
     heapq.heappush(heap, third_largest)
+
+
+def __init__(self):
+    self.h = []
+
+
+def minHeapify(self,i):
+    smallest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+    if left < len(self.h) and self.h[left] < self.h[smallest]:
+        smallest = left
+    if right < len(self.h) and self.h[right] < self.h[smallest]:
+        smallest = right
+    if smallest != i:
+        self.h[i], self.h[smallest] = self.h[smallest], self.h[i]
+        minHeapify(smallest)
+
+
+def buildHeap(n):
+    for i in range(n // 2 - 1, -1, -1):
+        minHeapify(i)
+
+
+def top(self):
+    return self.h[0]
