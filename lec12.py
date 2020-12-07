3391c11d277a54083aac69be8b10d3bc2afd5eed
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-12-07 14:51:28 +0700

    Update lec12.py

--- lec12.py
+++ lec12.py
@@ -58,33 +58,81 @@
 #Pizzamania
 
 
-def findPair(data,length,sum):
-    num_pair = 0
-    if length < 1:
-        return num_pair
-    left = 0
-    right = length-1
-    while right > left:
-        curSum = data[right] + data[left]
-        if curSum == sum:
-            num_pair+=1
-            right+=1
-            left-=1
-        elif curSum > sum:
-            right -=1
-        else:
-            left +=1
+# def findPair(data,length,sum):
+#     num_pair = 0
+#     if length < 1:
+#         return num_pair
+#     behind = 0
+#     ahead = length-1
+#     while ahead > behind:
+#         curSum = data[ahead] + data[behind]
+#         if curSum == sum:
+#             num_pair+=1
+#             behind+=1
+#             ahead-=1
+#         elif curSum > sum:
+#             ahead -=1
+#         else:
+#             behind +=1
+#
+#     return num_pair
+#
+# def main():
+#     test = int(input())
+#     while test > 0:
+#         n,m = map(int,input().split())
+#         arr = list(map(int, input().split()))
+#         sorted(arr)
+#         print(findPair(arr,n,m))
+#         test -=1
+#
+#
+# if __name__ == '__main__':
+#     main()
 
-    return num_pair
+#The Playboy Chimp
+
+def binSearch(a, length, val):
+    left, right = 0, length - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if a[mid] == val:
+            return mid - 1, mid + 1
+        if a[mid] < val:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return right, left
+
 
 def main():
-    test = int(input())
-    while test > 0:
-        n,m = map(int,input().split())
-        arr = list(map(int, input().split()))
-        sorted(arr)
-        findPair(arr,n,m)
-        test -=1
+
+    n = int(input())
+    height = list(map(int,input().split()))
+    sorted(height)
+
+    chimps = []
+    last = -1
+
+    for x in height:
+        if x != last:
+            chimps.append(x)  # Keeps only the unique elements.
+            last = x
+    q = int(input())
+    queries = list(map(int,input().split()))
+    sorted(queries)
+
+    sz = len(chimps)
+
+
+    for q in queries:
+        a, b = binSearch(chimps, sz, q)
+        tallest = str(chimps[a]) if a >= 0 else 'X'
+        shortest = str(chimps[b]) if b < sz else 'X'
+        print(tallest,shortest)
 
 
 if __name__ == '__main__':
