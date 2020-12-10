b1bcdb0422f153811f35b87303ef46771cef1d69
vuminhdiep <vuminhdiep.hycmd@gmail.com>
2020-12-10 15:31:50 +0700

    Add intepreter python

--- lec13.py
+++ lec13.py
@@ -12,16 +12,16 @@
 #         print('Average')
 
 #Monk and his Friends
-t=int(input())
-for i in range(t):
-    n,m=list(map(int,input().split()))
-    arr=list(map(int,input().split()))
-    dic=set()
-    for i in range(n):
-        dic.add(arr[i])
-    for i in range(n,n+m):
-        if arr[i] in dic:
-            print("YES")
-        else:
-            print("NO")
-        dic.add(arr[i])
+# t=int(input())
+# for i in range(t):
+#     n,m=list(map(int,input().split()))
+#     arr=list(map(int,input().split()))
+#     dic=set()
+#     for i in range(n):
+#         dic.add(arr[i])
+#     for i in range(n,n+m):
+#         if arr[i] in dic:
+#             print("YES")
+#         else:
+#             print("NO")
+#         dic.add(arr[i])
