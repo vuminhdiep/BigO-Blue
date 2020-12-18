04642e1573e4aa366017817256e79f9b39973cba
Vu Minh Diep <vuminhdiep.hycmd@gmail.com>
2020-12-18 15:59:27 +0700

    Update lec15.py

--- lec15.py
+++ lec15.py
@@ -1,4 +1,20 @@
 #MST
+import queue
+INF = 1e9
+class Node:
+    def __init__(self,index,dist):
+        self.index = index
+        self.dist = dist
+    def __lt__(self, other):
+        return self.dist <= other.weight
 n, m = map(int,input().split())
+graph = []
 for i in range(m):
-    i,j,k = map(int,input().split())
+    u,v,w = map(int,input().split())
+    u -= 1
+    v -= 1
+    graph[u].append(Node(v, w))
+    graph[v].append(Node(u, w))
+
+res = Prim(0)
+print(res)
